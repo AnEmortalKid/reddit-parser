03d0143d7846be92118a23369fa9d1fb5050a225
AnEmortalKid <janmonterrubio@gmail.com>
2015-07-28 21:24:21 -0500

    Adding new files

---
+++ src/main/java/com/anemortalkid/reddit/parser/dataobjects/DataObject.java
@@ -0,0 +1,22 @@
+package com.anemortalkid.reddit.parser.dataobjects;
+
+public interface DataObject {
+
+	String toHTMLTableRow();
+
+	default String wrapInTd(String str) {
+		return "\t<td>" + str + "</td>";
+	}
+
+	default String wrapInTr(String str) {
+		return "<tr>" + str + "\n</tr>";
+	}
+
+	default String toTdTr(String... tdElems) {
+		StringBuilder bob = new StringBuilder();
+		for (String tdElem : tdElems) {
+			bob.append("\n" + wrapInTd(tdElem));
+		}
+		return wrapInTr(bob.toString());
+	}
+}
