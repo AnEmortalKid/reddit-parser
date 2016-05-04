c168f7af5e85504d0a774fcc72694cd39880bb12
AnEmortalKid <janmonterrubio@gmail.com>
2016-05-03 20:02:41 -0500

    adding villain code

--- src/main/java/com/anemortalkid/reddit/parser/sitebuilder/villains/VillainsSiteBuilder.java
+++ src/main/java/com/anemortalkid/reddit/parser/sitebuilder/villains/VillainsSiteBuilder.java
@@ -1,5 +1,6 @@
 package com.anemortalkid.reddit.parser.sitebuilder.villains;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import com.anemortalkid.reddit.parser.sitebuilder.BaseSiteBuilderHelper;
@@ -14,20 +15,26 @@ public class VillainsSiteBuilder implements ISiteBuilder {
 		String redditURL = "https://www.reddit.com/r/DnDBehindTheScreen/comments/3fmnhb/10000_villains/";
 		StrongParagraphScrubber scrubber = new StrongParagraphScrubber(
 				redditURL);
-		List<ScrubbedDataObject> data = scrubber.scrubData();
+		List<ScrubbedDataObject> oldData = scrubber.scrubData();
+		System.out.println("Villains scrubbed Old: " + oldData.size());
 
-		System.out.println("Villains scrubbed: " + data.size());
-
+		String newURL = "https://www.reddit.com/r/DnDBehindTheScreen/comments/4hn60f/10k_villains_demons_and_devils/";
+		StrongParagraphScrubber newDataScrubber = new StrongParagraphScrubber(newURL);
+		List<ScrubbedDataObject> newData = newDataScrubber.scrubData();
+		System.out.println("New Data Scrubbed: " + newData.size());
+		List<ScrubbedDataObject> allData = new ArrayList<>(oldData);
+		allData.addAll(newData);
+		
 		String fileLocationAndName = "src/main/resources/villains";
-		scrubber.writeDataToFiles(fileLocationAndName, data);
+		scrubber.writeDataToFiles(fileLocationAndName, allData);
 
 		String folder = fileLocationAndName + "/";
 
 		String header = "<tr><th align=\"center\">Villain Name</th><th align=\"center\">Description</th></tr>";
-		new BaseSiteBuilderHelper(folder, "Villains", redditURL, header, data)
+		new BaseSiteBuilderHelper(folder, "Villains", newURL, header, allData)
 				.buildHTML();
 
-		scrubber.writeDataToFiles(fileLocationAndName, data);
+		scrubber.writeDataToFiles(fileLocationAndName, allData);
 	}
 
 	public static void main(String[] args) {
